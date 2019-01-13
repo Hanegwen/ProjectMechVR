f3ce4bfb8d99c6e26be66f958b9d403c14097f98
Mike Morrone <Hanegwen725@gmail.com>
2019-01-13 08:23:13 -0600

    Added Bullet Nonsence

--- MechVR/Assets/Scripts/Bullets/baseBullet.cs
+++ MechVR/Assets/Scripts/Bullets/baseBullet.cs
@@ -6,8 +6,12 @@ public class baseBullet : MonoBehaviour
 {
     float lifetime = 5;
     float movementSpeed = 100;
+    int damage = 1;
 
     Rigidbody rigidbody;
+
+    enum owners {player, enemy };
+    owners myOwner;
     // Start is called before the first frame update
     void Start()
     {
@@ -34,6 +38,29 @@ public class baseBullet : MonoBehaviour
         if(lifetime <= 0 )
         {
             Destroy(this.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.GetComponent<IDamagable>() != null)
+        {
+            if (other.gameObject.name.Contains("Player"))
+            {
+                if (myOwner == owners.enemy)
+                {
+                    other.gameObject.GetComponent<IDamagable>().TakeDamage(damage);
+                    print("Bullet hits Player");
+                }
+            }
+            else if (other.gameObject.name.Contains("Enemy"))
+            {
+                if (myOwner == owners.player)
+                {
+                    other.gameObject.GetComponent<IDamagable>().TakeDamage(damage);
+                    print("Bullet hits Enemy");
+                }
+            }
         }
     }
 }
